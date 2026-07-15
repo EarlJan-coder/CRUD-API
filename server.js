c787df31c49d2c377ffcb49ccaada0dae4bd0e91
EarlJan-coder <earljhonamalatag2@gmail.com>
2026-07-15 22:54:53 +0800

    Added swaggerui for docs

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocument from './openapi.json' with { type: "json" }
 
 import tasksRoutes from './routes/tasks.js'
 
@@ -9,6 +11,7 @@ const PORT = 3000
 app.use(bodyParser.json())
 
 app.use('/tasks', tasksRoutes)
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.get("/", (req, res) => {
     console.log('[GET ROUTE]')
